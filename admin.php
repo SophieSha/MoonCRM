18f42517b3d74e3d06583146b127abbb1bd6c3d6
Sophie Sha <s3742024@student.rmit.edu.au>
2019-05-27 14:40:39 +1000

    fix config

--- admin.php
+++ admin.php
@@ -1,14 +1,12 @@
 <?php
-// // Initialize the session
-// session_start();
+// Initialize the session
+session_start();
 
-// // Check if the user is logged in, if not then redirect him to login page
-// if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
-//     header("location: login.php");
-//     exit;
-// }
-
-require_once 'config.php';
+// Check if the user is logged in, if not then redirect him to login page
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    header("location: login.php");
+    exit;
+}
 ?>
 
 
